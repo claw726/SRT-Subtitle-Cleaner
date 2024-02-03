237b9c8b0b4a491c66e71deb22f8b47f6dca8d2f
Charles Law <calaw726@gmail.com>
2024-02-03 11:56:28 -0500

    Added Clap

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::env;
 use std::fs;
 use std::io;
+use std::path::PathBuf;
 use regex::Regex;
+use clap::{App, Arg};
 
-fn remove_css_font_styling(file_path: &str) -> io::Result<()> {
+fn remove_css_font_styling(file_path: &PathBuf) -> io::Result<()> {
     let content = fs::read_to_string(file_path)?;
 
     // Remove CSS Font Styling
@@ -12,24 +13,27 @@ fn remove_css_font_styling(file_path: &str) -> io::Result<()> {
 
     fs::write(file_path, cleaned_content)?;
 
-    println!("CSS Font Styling removed from {}", file_path);
+    println!("CSS Font Styling removed from {}", file_path.to_str().unwrap());
 
     Ok(())
 }
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("SRT Subtitle Cleaner")
+        .version("1.0")
+        .author("claw726")
+        .about("Remove CSS Font Styling from SRT Subtitle files")
+        .arg(Arg::with_name("file_path")
+            .help("Path to the SRT file")
+            .required(true)
+            .index(1))
+        .get_matches();
 
-    if args.len() != 2 {
-        eprintln!("Usage: {} <file_path>", args[0]);
-        std::process::exit(1);
-    }
+    let file_path = PathBuf::from(matches.value_of("file_path").unwrap());
 
-    let file_path = &args[1];
-
-    if let Err(err) = remove_css_font_styling(file_path) {
+    remove_css_font_styling(&file_path).unwrap_or_else(|err|{
         eprintln!("Error: {}", err);
         std::process::exit(1);
-    }
+    }); 
 }
